e4d841a6d1d3cfca46f45bda440cfb307c6147bf
rafsanjani rahadi <rahadirafsanjani@gmail.com>
2022-09-21 19:49:10 +0700

    [add]web-blog-app: setup letter oppener and installing cucumber

--- web-blog-app/config/routes.rb
+++ web-blog-app/config/routes.rb
@@ -1,5 +1,5 @@
   Rails.application.routes.draw do
-    devise_for :users
+    devise_for :users, controllers: { confirmations: 'users/confirmations' }
     resources :posts do
       resources :comments
     end
@@ -7,7 +7,6 @@
     root "home#root"
     get '/comments/:id/edit', to: 'comments#edit', as: :edit_comment
     put '/comments/:id/edit', to: 'comments#update', as: :update_comment
-
     delete '/comments/:id/destroy', to: 'comments#destroy', as: :destroy_comment
 
 
